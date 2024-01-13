57309558733ab5e4134f1f4fe086783e150efa65
indexedakki <74480055+indexedakki@users.noreply.github.com>
2024-01-13 14:08:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 "use client"
 import React, { useState } from 'react';
-import ChangeEvent from 'react';
+import React, { useState, ChangeEvent } from 'react';
  
 const UploadPage = () => {
   const [selection, setSelection] = useState<string | null>('');
