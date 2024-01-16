b6405bf6bbb43ef8a2c04672bb485b5929d2e04d
indexedakki <74480055+indexedakki@users.noreply.github.com>
2024-01-16 08:48:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,21 @@
 "use client"
 
 import React, { useState, ChangeEvent } from 'react';
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.use((req, res, next) => {
-//  res.header("Access-Control-Allow-Origin", "pypypy12.azurewebsites.net"); // replace * with your domain
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//  next();
-// });
+app.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "pypypy12.azurewebsites.net"); // replace * with your domain
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+ next();
+});
+//  app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// })
  
 const UploadPage = () => {
   const [selection, setSelection] = useState<string | null>('');
